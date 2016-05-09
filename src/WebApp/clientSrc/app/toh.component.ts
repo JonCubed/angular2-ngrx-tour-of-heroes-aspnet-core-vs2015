32475ab8c6fb5103976b15f86eafe6441754aea1
Jonathan Kuleff <jonathankuleff@gmail.com>
2016-05-10 01:23:31 +1000

    Update App Component

--- src/WebApp/clientSrc/app/toh.component.ts
+++ src/WebApp/clientSrc/app/toh.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Hero } from './shared/hero.model';
 
 @Component({
   moduleId: module.id,
@@ -7,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrls: ['toh.component.css']
 })
 export class TohAppComponent {
-  title = 'toh works!';
+    title = 'Tour of Heroes';
+    selectedHero: Hero;
+    public heroes = HEROES;
+    
+    onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+var HEROES: Hero[] = [
+  { "id": 11, "name": "Mr. Nice" },
+  { "id": 12, "name": "Narco" },
+  { "id": 13, "name": "Bombasto" },
+  { "id": 14, "name": "Celeritas" },
+  { "id": 15, "name": "Magneta" },
+  { "id": 16, "name": "RubberMan" },
+  { "id": 17, "name": "Dynama" },
+  { "id": 18, "name": "Dr IQ" },
+  { "id": 19, "name": "Magma" },
+  { "id": 20, "name": "Tornado" }
+];
